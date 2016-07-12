8163a7596f580d3060f7d5895e4861d426cf663c
keletso <patrickkeletso@gmail.com>
2016-07-12 13:01:26 +0200

    setup for map

--- src/systemjs.config.js
+++ src/systemjs.config.js
@@ -1,43 +1,48 @@
 (function(global) {
-
-    // map tells the System loader where to look for things
     var map = {
         'app':                        'app', // 'dist',
+        '@angular':                   'node_modules/@angular',
+        'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
         'rxjs':                       'node_modules/rxjs',
-        '@angular':                   'node_modules/@angular'
+        'materialize-css': 'node-modules/materialize-css',
+        'materialize': 'node_modules/angular2-materialize',
+        'angular2-materialize': 'node_modules/angular2-materialize'
     };
-
-    // packages tells the System loader how to load when no filename and/or no extension
+	
     var packages = {
         'app':                        { main: 'main.js',  defaultExtension: 'js' },
-        'rxjs':                       { defaultExtension: 'js' }
+        'rxjs':                       { defaultExtension: 'js' },
+        'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' },
+        'materialize-css': { 'main': 'dist/js/materialize'},
+        'materialize': { 'main': 'dist/materialize-directive', 'defaultExtension': 'js'}
     };
-
     var ngPackageNames = [
-        '@angular/common',
-        '@angular/compiler',
-        '@angular/core',
-        '@angular/http',
-        '@angular/forms',
-        '@angular/platform-browser',
-        '@angular/platform-browser-dynamic',
-        '@angular/router',
-        '@angular/testing'
+        'common',
+        'compiler',
+        'core',
+        'forms',
+        'http',
+        'platform-browser',
+        'platform-browser-dynamic',
+        'router',
+        'router-deprecated',
+        'upgrade',
     ];
 
-    // add package entries for angular packages in the form '@angular/common': { main: 'index.js', defaultExtension: 'js' }
-    ngPackageNames.forEach(function(pkgName) {
-        packages[pkgName] = { main: 'index.js', defaultExtension: 'js' };
-    });
+    function packIndex(pkgName) {
+        packages['@angular/'+pkgName] = { main: 'index.js', defaultExtension: 'js' };
+    }
 
+    function packUmd(pkgName) {
+        packages['@angular/'+pkgName] = { main: '/bundles/' + pkgName + '.umd.js', defaultExtension: 'js' };
+    }
+	
+    var setPackageConfig = System.packageWithIndex ? packIndex : packUmd;
+	
+    ngPackageNames.forEach(setPackageConfig);
     var config = {
         map: map,
         packages: packages
-    }
-
-    // filterSystemConfig - index.html's chance to modify config before we register it.
-    if (global.filterSystemConfig) { global.filterSystemConfig(config); }
-
+    };
     System.config(config);
-
 })(this);
