3bf0bdbef8f9e766ce18c7aeebbe5e05b5d6d994
NsovoBaloyi <nsovo.baloyi23@gmail.com>
2016-07-14 13:21:51 +0200

    Added script to clean .map and .js files

---
+++ clean.sh
@@ -0,0 +1,12 @@
+cd src/app/
+rm *.js *.map
+cd disaster-center/
+rm *.js *.map
+cd ../login/
+rm *.js *.map
+cd ../map/
+rm *.js *.map
+cd ../register/
+rm *.js *.map
+cd ../services/
+rm *.js *.map
