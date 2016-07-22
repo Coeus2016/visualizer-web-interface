24d9b50321635318712ae2712597e5089ee6f8d0
NsovoBaloyi <nsovo.baloyi23@gmail.com>
2016-07-22 12:31:10 +0200

    Addes map service

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,28 +1,18 @@
 import {Component, OnInit} from '@angular/core';
+import {MapService} from "../services/map.service";
 
-declare var ol: any;
 
 @Component({
     selector: 'my-map',
-templateUrl: 'app/map/map.component.html'
+    providers: [MapService],
+    templateUrl: 'app/map/map.component.html'
 })
 
 export class MapComponent implements OnInit{
     ol: any;
-    constructor(){
+    constructor(private _mapService: MapService){
     }
     ngOnInit(){
-        var map = new ol.Map({
-            target: 'map',
-            layers: [
-                new ol.layer.Tile({
-                    source: new ol.source.OSM()
-                })
-            ],
-            view: new ol.View({
-                center: ol.proj.fromLonLat([37.41, 8.82]),
-                zoom: 10
-            })
-        });
+        this._mapService.getMap();
     }
 }
