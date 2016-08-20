549120929d81f7528898b539a2a3f328ab0c3cb5
NsovoBaloyi <nsovo.baloyi23@gmail.com>
2016-08-20 22:18:28 +0200

    Changes angular2 to angular1

--- karma.conf.js
+++ karma.conf.js
@@ -1,79 +1,32 @@
 module.exports = function(config) {
     config.set({
 
-    basePath: '',
+    basePath: './app',
 
 
     frameworks: ['jasmine'],
         plugins: [
-            require('karma-jasmine'),
-            require('karma-phantomjs-launcher'),
-            require('karma-coverage')
+            'karma-jasmine',
+            'karma-phantomjs-launcher',
+            'karma-coverage'
         ],
-
-        customLaunchers: {
-            // From the CLI. Not used here but interesting
-            // chrome setup for travis CI using chromium
-            Chrome_travis_ci: {
-                base: 'Chrome',
-                flags: ['--no-sandbox']
-            }
-        },
 
     files: [
 
-        'node_modules/systemjs/dist/system.src.js',
-        'node_modules/systemjs/dist/system-polyfills.js',
-
-
-
-
-
-        // Polyfills
-        'node_modules/core-js/client/shim.min.js',
-
-
-        // Reflect and Zone.js
-        'node_modules/reflect-metadata/Reflect.js',
-        'node_modules/zone.js/dist/zone.js',
-        'node_modules/zone.js/dist/jasmine-patch.js',
-        'node_modules/zone.js/dist/async-test.js',
-        'node_modules/zone.js/dist/fake-async-test.js',
-
-        // RxJs.
-        { pattern: 'node_modules/rxjs/**/*.js', included: false, watched: false },
-        { pattern: 'node_modules/rxjs/**/*.js.map', included: false, watched: false },
-
-        // Angular 2 itself and the testing library
-        {pattern: 'node_modules/@angular/**/*.js', included: false, watched: false},
-        {pattern: 'node_modules/@angular/**/*.js.map', included: false, watched: false},
-
-        {pattern: 'src/systemjs.config.js', included: false, watched: false},
-        'src/karma-test-shim.js',
-       // {pattern: 'karma-test-shim.js', included: true, watched:true},
-        {pattern: 'src/app/**/*.js', included: false, watched: true},
-
-
-
-        // paths loaded via Angular's component compiler
-        // (these paths need to be rewritten, see proxies section)
-        {pattern: 'src/app/**/*.html', included: false, watched: true},
-        {pattern: 'src/app/**/*.css', included: false, watched: true},
-
-        // paths to support debugging with source maps in dev tools
-        {pattern: 'src/app/**/*.ts', included: false, watched: false},
-        {pattern: 'src/app/**/*.js.map', included: false, watched: false}
-    ],
-     proxies: {
-         "/app/": '/base/src/app/'
-     },
+        'bower_components/angular/angular.js',
+        "bower_components/angular-resource/angular-resource.js",
+        "bower_components/angular-loader/angular-loader.js",
+        "bower_components/angular-sanitize/angular-sanitize.js",
+        'bower_components/angular-route/angular-route.js',
+        'bower_components/angular-mocks/angular-mocks.js',
+        'test*/**/*.js'    ],
 
     preprocessors: {
-        'src/app/**/!(*spec).js': ['coverage']
+        'test*/**/*.js': ['coverage']
     },
 
     coverageReporter: {
-        dir : 'coverage/',
+        dir : '../coverage/',
 	    reporters: [
 	        { type: 'json'},
             { type: 'html'},
@@ -86,8 +39,8 @@ module.exports = function(config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: false,
+    autoWatch: true,
     browsers: ['PhantomJS'],
     singleRun: true
   })
-}
+};
