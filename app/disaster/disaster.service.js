d0241abf81b7632b53495c2cea95ca691f517a8f
NsovoBaloyi <nsovo.baloyi23@gmail.com>
2016-08-21 15:41:17 +0200

    Addes disaster service and module

---
+++ app/disaster/disaster.service.js
@@ -0,0 +1,13 @@
+angular.
+module('disaster').
+factory('Disaster', ['$resource',
+    function($resource) {
+        return $resource('phones/:phoneId.json', {}, {
+            query: {
+                method: 'GET',
+                params: {phoneId: 'phones'},
+                isArray: true
+            }
+        });
+    }
+]);
