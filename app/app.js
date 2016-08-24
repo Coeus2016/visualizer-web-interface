167796ec27e40504e0b280910fd9b7d2cb67bb52
keletso <patrickkeletso@gmail.com>
2016-08-24 21:06:47 +0200

    add autocomplete functionality

--- app/app.js
+++ app/app.js
@@ -10,20 +10,7 @@ myApp.config(function($locationProvider,$mdThemingProvider) {
     .accentPalette('orange');
 });
 
-function AppCtrl ($timeout, $q, $log,$scope) {
-  $scope.user = {
-    title: 'Developer',
-    email: 'ipsum@lorem.com',
-    firstName: '',
-    lastName: '',
-    company: 'Google',
-    address: '1600 Amphitheatre Pkwy',
-    city: 'Mountain View',
-    state: 'CA',
-    biography: 'Loves kittens, snowboarding, and can type at 130 WPM.\n\nAnd rumor has it she bouldered up Castle Craig!',
-    postalCode: '94043'
-  };
-  
+function AppCtrl ($timeout, $q, $log,$scope,$http) {
   $scope.geospatial = ('weather disasters').split(' ').map(function(gisdata) {
     return {data: gisdata};
   });
@@ -31,7 +18,6 @@ function AppCtrl ($timeout, $q, $log,$scope) {
   var self = this;
   self.simulateQuery = false;
   self.isDisabled    = false;
-  self.repos         = loadAll();
   self.querySearch   = querySearch;
   self.selectedItemChange = selectedItemChange;
   self.searchTextChange   = searchTextChange;
@@ -43,14 +29,11 @@ function AppCtrl ($timeout, $q, $log,$scope) {
     * remote dataservice call.
     */
   function querySearch (query) {
-    var results = query ? self.repos.filter( createFilterFor(query) ) : self.repos,deferred;
-    if (self.simulateQuery) {
-      deferred = $q.defer();
-      $timeout(function () { deferred.resolve( results ); }, Math.random() * 1000, false);
-      return deferred.promise;
-    } else {
-      return results;
-    }
+    return $http.get('http://photon.komoot.de/api/?limit=5&q=' + escape(query))
+                .then(function(result) {
+                    self.data = result.data.features;
+                    return result.data.features;
+                  });
   }
   
   function searchTextChange(text) {
@@ -59,56 +42,5 @@ function AppCtrl ($timeout, $q, $log,$scope) {
   
   function selectedItemChange(item) {
     $log.info('Item changed to ' + JSON.stringify(item));
-  }
-  
-  /**
-    * Build `components` list of key/value pairs
-    */
-  function loadAll() {
-    var repos = [{
-      'name'      : 'Angular 1',
-      'url'       : 'https://github.com/angular/angular.js',
-      'watchers'  : '3,623',
-      'forks'     : '16,175',
-    },
-    {
-      'name'      : 'Angular 2',
-      'url'       : 'https://github.com/angular/angular',
-      'watchers'  : '469',
-      'forks'     : '760',
-    },
-    {
-      'name'      : 'Angular Material',
-      'url'       : 'https://github.com/angular/material',
-      'watchers'  : '727',
-      'forks'     : '1,241',
-    },
-    {
-      'name'      : 'Bower Material',
-      'url'       : 'https://github.com/angular/bower-material',
-      'watchers'  : '42',
-      'forks'     : '84',
-    },
-    {
-      'name'      : 'Material Start',
-      'url'       : 'https://github.com/angular/material-start',
-      'watchers'  : '81',
-      'forks'     : '303',
-    }];
-    
-    return repos.map( function (repo) {
-      repo.value = repo.name.toLowerCase();
-      return repo;
-    });
-  }
-  
-  /**
-    * Create filter function for a query string
-    */
-  function createFilterFor(query) {
-    var lowercaseQuery = angular.lowercase(query);
-    return function filterFn(item) {
-      return (item.value.indexOf(lowercaseQuery) === 0);
-    };
   }
 }
