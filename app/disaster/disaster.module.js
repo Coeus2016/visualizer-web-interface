4c80e104365453ce0cdc4aa263514af0188dde7e
NsovoBaloyi <nsovo.baloyi23@gmail.com>
2016-08-25 12:10:53 +0200

    Disaster service

--- app/disaster/disaster.module.js
+++ app/disaster/disaster.module.js
@@ -1,1 +1,31 @@
-angular.module('disaster', ['ngResource']);
+var myDisaster = angular.module('disaster', [''
+])
+.service('DisasterService',function(){
+    this.getEarthquakes = function(info){
+
+    };
+
+    this.getFire = function(info){
+
+    };
+})
+.controller('DisasterCtrl',DisasterCtrl);
+
+function DisasterService(){
+    getEarthquakes(info){
+
+    }
+}
+
+function DisasterCtrl($scope,MapService, DisasterService){
+
+    function disaplyData(){
+        DisasterService.getEarthquakes(info);
+
+
+    }
+
+}
+
+
+
