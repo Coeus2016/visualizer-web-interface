a046bebc98624ff150d34db06944565852338879
NsovoBaloyi <nsovo.baloyi23@gmail.com>
2016-09-06 15:02:45 +0200

    Changes from OpenLayers to Leafletjs

--- app/core/map/map.js
+++ app/core/map/map.js
@@ -27,13 +27,41 @@ myMap.service('MapService',function(){
 
     this.addLayer = function(longitude,latitude){
         this.markers.addLayer(L.marker([latitude,longitude]));
-    }
+    };
+
 
     this.updateLocation = function(longitude, latitude){
         this.map.panTo(new L.LatLng(latitude,longitude));
         self.longitude=longitude;
         self.latitude=latitude;
-    }
+    };
+
+    this.addEarthLayer =  function(result){
+
+        //console.log(result.data.length);
+       for( var i = 0; i < result.data.length; i++){
+            //console.log(result.data[i].geometry.coordinates[0]);
+            this.addLayer(result.data[i].geometry.coordinates[0],result.data[i].geometry.coordinates[1]);
+       }
+
+    };
+    this.removeEarthLayer =  function(){
+
+    };
+
+    this.addFireLayer =  function(result){
+
+        //console.log(result.data.length);
+        for( var i = 0; i < result.data.length; i++){
+            this.addLayer(result.data[i].longitude,result.data[i].longitude);
+        }
+
+    };
+    this.removeFireLayer =  function(){
+
+    };
+
+
 });
 
 myMap.controller('MapCtrl', MapCtrl);
