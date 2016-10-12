035d67290274d9c982ceaf7d12c14528539196cf
keletso <patrickkeletso@gmail.com>
2016-10-12 21:10:20 +0200

    ngmessages login form

--- app/home/index.js
+++ app/home/index.js
@@ -1,7 +1,7 @@
 var myApp = angular.module('welcome', ['ngMaterial','ngMessages']);
 myApp.controller('WelcomeCtrl',WelcomeCtrl);
 
-function WelcomeCtrl($scope, $mdDialog){
+function WelcomeCtrl($scope, $mdDialog,$mdToast){
 	$scope.showSignUp = function(ev){
     $mdDialog.show({
       controller: DialogController,
@@ -10,11 +10,6 @@ function WelcomeCtrl($scope, $mdDialog){
       targetEvent: ev,
       clickOutsideToClose:true,
   		fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
-    })
-    .then(function(answer) {
-  		$scope.status = 'You said the information was "' + answer + '".';
-    }, function() {
-      $scope.status = 'You cancelled the dialog.';
     });
   };
 
@@ -26,56 +21,49 @@ function WelcomeCtrl($scope, $mdDialog){
   		targetEvent: ev,
       clickOutsideToClose:true,
       fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
-    })
-    .then(function(answer){
-      $scope.status = 'You said the information was "' + answer + '".';
-    }, function() {
-      $scope.status = 'You cancelled the dialog.';
     });
   };
 
   function DialogController($scope, $mdDialog,$http,store,$state){
     $scope.user = {};
 
-    $scope.hide = function() {
-      console.log($scope.user);
+    $scope.hide = function(){
       $mdDialog.hide();
     };
 
-    $scope.cancel = function() {
-      console.log($scope.user);
+    $scope.cancel = function(){
       $mdDialog.cancel();
     };
 
     $scope.login = function(){
+      $scope.loginform.$setValidity("emailpassword", true);
       $http({
         url: 'http://localhost:3300/login',
         method: 'POST',
         data: $scope.user
       }).then(function(response) {
         store.set('jwt', response.data.message);
-        $scope.hide();
+        $scope.cancel();
         $state.go('main',{},{ reload: true });
       }, function(error) {
-        console.log(error.data);
+        $mdToast.show($mdToast.simple().textContent('email or password incorrect.'));
+        $scope.loginform.$setValidity("emailpassword", false);
       });
     };
 
     $scope.register = function(){
+      $scope.signupform.$setValidity("userexists", true);
       $http({
         url:'http://localhost:3300/register',
         method: 'POST',
         data: $scope.user
       }).then(function(response){
-        console.log(response.data.message);
+        $mdToast.show($mdToast.simple().textContent('Thank you for signing up.'));
+        $scope.cancel();
       }, function(error){
-        console.log(error.data);
+        $mdToast.show($mdToast.simple().textContent('email already exist.'));
+        $scope.signupform.$setValidity("userexists", false);
       });
     }
-
-    $scope.answer = function(answer) {
-      console.log($scope.user);
-      $mdDialog.hide(answer);
-    };
   }
 }
